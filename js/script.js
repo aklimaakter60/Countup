0263dcaf1d34bf10ed86060058ec175f08ad4ea6
aklimaakter <aklimaaku123@gmail.com>
2025-07-19 18:08:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+//counter up
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+//count-up-me
+$("#counter").countMe(40,.5);
+
+//number animate
+$('.js-counter').numberAnimate({
+  duration: 3000
+});
